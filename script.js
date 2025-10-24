a0e6d4db43e6ef7b80f751294b575faefdba5a10
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-10-24 11:41:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const div=document.getElementById("output");
+const message=document.getElementById("text");
+const delay=document.getElementById("delay");
+const btn=document.getElementById("btn");
+
+btn.addEventListener("click",()=>{
+	setTimeout(()=>{
+		div.textContent=message.value
+	},parseInt(`${delay.value}`))
+	
+})
